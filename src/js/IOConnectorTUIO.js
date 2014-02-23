245b5f45d2d1bd2919550605e535d736a4ed2f47
Philipp Fischer <philipp.fischer@daimler.com>
2014-02-22 18:51:26 -0800

    added standard TUIO functions

--- src/js/IOConnectorTUIO.js
+++ src/js/IOConnectorTUIO.js
@@ -153,12 +153,12 @@ $(function() {
     };
     
     client.on("connect", onConnect);
-//    client.on("addTuioCursor", onAddTuioCursor);
-//    client.on("updateTuioCursor", onUpdateTuioCursor);
-//    client.on("removeTuioCursor", onRemoveTuioCursor);
-//    client.on("addTuioObject", onAddTuioObject);
-//    client.on("updateTuioObject", onUpdateTuioObject);
-//    client.on("removeTuioObject", onRemoveTuioObject);
+    client.on("addTuioCursor", onAddTuioCursor);
+    client.on("updateTuioCursor", onUpdateTuioCursor);
+    client.on("removeTuioCursor", onRemoveTuioCursor);
+    client.on("addTuioObject", onAddTuioObject);
+    client.on("updateTuioObject", onUpdateTuioObject);
+    client.on("removeTuioObject", onRemoveTuioObject);
 //    client.on("refresh", onRefresh);
         client.connect();
 });
