6c82939252dffaef0f7bdf80c8aebdc5ff5ed1fa
Philipp Fischer <philipp.fischer@daimler.com>
2014-02-22 18:39:25 -0800

    test TUIO Connector

--- src/js/IOConnectorTUIO.js
+++ src/js/IOConnectorTUIO.js
@@ -146,14 +146,14 @@ $(function() {
     };
     
     client.on("connect", onConnect);
-    client.on("addTuioCursor", onAddTuioCursor);
-    client.on("updateTuioCursor", onUpdateTuioCursor);
-    client.on("removeTuioCursor", onRemoveTuioCursor);
-    client.on("addTuioObject", onAddTuioObject);
-    client.on("updateTuioObject", onUpdateTuioObject);
-    client.on("removeTuioObject", onRemoveTuioObject);
-    client.on("refresh", onRefresh);
-    //    client.connect();
+//    client.on("addTuioCursor", onAddTuioCursor);
+//    client.on("updateTuioCursor", onUpdateTuioCursor);
+//    client.on("removeTuioCursor", onRemoveTuioCursor);
+//    client.on("addTuioObject", onAddTuioObject);
+//    client.on("updateTuioObject", onUpdateTuioObject);
+//    client.on("removeTuioObject", onRemoveTuioObject);
+//    client.on("refresh", onRefresh);
+        client.connect();
 });
 
 
