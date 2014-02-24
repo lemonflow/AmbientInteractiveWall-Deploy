e628d64dac3d04349834c3e6b0c5e6099911f891
Philipp Fischer <philipp.fischer@daimler.com>
2014-02-23 21:07:00 -0800

    added Touch Connector

---
+++ src/js/IOConnectorWinTouch.js
@@ -0,0 +1,13 @@
+//class with coverflow and operators
+
+var IOConnectorWinTouch;
+IOConnectorWinTouch = (function () {
+
+    function IOConnectorWinTouch() {
+    }
+
+    IOConnectorWinTouch.prototype.init = function () {
+    };
+
+    return IOConnectorWinTouch;
+})();
