42d4e9d928c0793536240808d8469392c488696b
Philipp Fischer <philipp.fischer@daimler.com>
2014-02-23 22:54:27 -0800

    drag picture

--- src/js/IOConnectorWinTouch.js
+++ src/js/IOConnectorWinTouch.js
@@ -2,6 +2,8 @@
 
 var IOConnectorWinTouch;
 IOConnectorWinTouch = (function () {
+    var currentTouches = new Array;
+
 
     function IOConnectorWinTouch() {
 //        document.addEventListener('click', function(e) {
@@ -12,44 +14,95 @@ IOConnectorWinTouch = (function () {
 
         document.addEventListener('touchstart', function(e) {
             e.preventDefault();
-            document.getElementById('debugtxt').textContent = "touchevent";
+            this.touchStarted(e);
+        }.bind(this), false);
 
-            coverflow.objects[0].position.x = -3000;
+        document.addEventListener('touchmove', function(e) {
+            e.preventDefault();
+            this.touchMoved(e);
+        }.bind(this), false);
 
-//            this.touchStarted(e);
-        }, false);
+        document.addEventListener('touchend', function(e) {
+            e.preventDefault();
+            this.touchEnded(e);
+        }.bind(this), false);
 
-//        document.body.addEventListener('touchmove', function(event) {
-//            event.preventDefault();
-//        }, false);
+        document.addEventListener('touchleave', function(e) {
+            e.preventDefault();
+            this.touchEnded(e);
+        }.bind(this), false);
     }
+
+
 
     IOConnectorWinTouch.prototype.init = function () {
 //        CoverFlow.objects[3].position.x = 2200;
 
     };
 
+    IOConnectorWinTouch.prototype.findCurrentTouchIndex = function(id) {
+        for (var i=0; i < currentTouches.length; i++) {
+            if (currentTouches[i].id === id) {
+                return i;
+            }
+        }
+        return -1;
+    };
+
     IOConnectorWinTouch.prototype.touchStarted = function (event) {
-        coverflow.objects[0].position.x = 200;
+        //-2560 until -1280
+        coverflow.objects[0].position.x = -2560;
 
         var touches = event.changedTouches;
 
         for (var i=0; i < touches.length; i++) {
             var touch = touches[i];
-            console.log(touch.pageX);
-//            var touchColor = randomColor();
-//
-//            currentTouches.push({
-//                id: touch.identifier,
-//                pageX: touch.pageX,
-//                pageY: touch.pageY,
-//                color: touchColor
-//            });
 
-//            ctx.beginPath();
-//            ctx.arc(touch.pageX, touch.pageY, 2.5, Math.PI*2, false);
-//            ctx.fillStyle = touchColor;
-//            ctx.fill();
+            currentTouches.push({
+                id: touch.identifier,
+                startX: touch.pageX,
+                startY: touch.pageY,
+                pageX: touch.pageX,
+                pageY: touch.pageY
+            });
+        }
+
+        document.getElementById('debugtxt').textContent = "add "+ touch.identifier+"@"+touch.pageX;
+
+    };
+
+    IOConnectorWinTouch.prototype.touchMoved = function (event) {
+        var touches = event.changedTouches;
+
+        for (var i=0; i < touches.length; i++) {
+            var touch = touches[i];
+            var currentTouchIndex = this.findCurrentTouchIndex(touch.identifier);
+
+            if (currentTouchIndex >= 0) {
+                var currentTouch = currentTouches[currentTouchIndex];
+                currentTouch.pageX = touch.pageX;
+                currentTouch.pageY = touch.pageY;
+                currentTouches.splice(currentTouchIndex, 1, currentTouch);
+
+                document.getElementById('debugtxt').textContent = "move"+currentTouch.startX;
+                coverflow.objects[0].position.x = -2560-(currentTouch.startX)+touch.pageX;
+            }
+
+        }
+    };
+
+    IOConnectorWinTouch.prototype.touchEnded = function (event) {
+        var touches = event.changedTouches;
+
+        for (var i=0; i < touches.length; i++) {
+            var touch = touches[i];
+            var currentTouchIndex = this.findCurrentTouchIndex(touch.identifier);
+
+            if (currentTouchIndex >= 0) {
+                var currentTouch = currentTouches[currentTouchIndex];
+                currentTouches.splice(currentTouchIndex, 1);
+                document.getElementById('debugtxt').textContent = "remove "+currentTouchIndex;
+            }
         }
     };
 
