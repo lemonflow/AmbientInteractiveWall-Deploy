4208e1da8fbcb88ac918feefdbca873b8898c0c2
Philipp Fischer <philipp.fischer@daimler.com>
2014-02-26 15:20:07 -0800

    go to slideck

--- src/js/components/CoverFlow/CoverFlowController.js
+++ src/js/components/CoverFlow/CoverFlowController.js
@@ -61,7 +61,7 @@ var CoverflowController = (function() {
                         guard:(function() {return (TouchDevice.currentY<200)}),
                         transition:[
                             CoverflowController.prototype.transitionHide,
-                            CoverflowController.prototype.transitionToFloorPlan
+                            CoverflowController.prototype.transitionToSlideDeck
                         ],
                         newState:"pre"
                     }
